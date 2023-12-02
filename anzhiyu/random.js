b7fefa46a660386c3e79b93f14761cd276438e59
冷醉泉 <2028983239@qq.com>
2023-12-02 17:30:44 +0800

    Site updated: 2023-12-02 17:30:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/4a05c739.html","posts/4fd0e23c.html"];function toRandomPost(){
+var posts=["posts/4a05c739.html","posts/4a17b156.html","posts/4fd0e23c.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
